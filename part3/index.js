f972063acd7eee89eb00ba67b8f109245d2038a9
Jonas Bäck <jonas.c.back@gmail.com>
2022-02-08 15:38:41 +0200

    Completed 3.4-3.6

--- part3/index.js
+++ part3/index.js
@@ -32,6 +32,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+})
+
+
 app.get('/info', (request, response) => {
   response.send(  
     `
@@ -39,6 +50,38 @@ app.get('/info', (request, response) => {
       <p>${new Date()}</p>
     `
   );
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+  response.status(204).end();
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    });
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: Math.ceil(Math.random() * 1000000),
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person);
+  console.log(person);
+  response.json(person);  
 })
 
 const PORT = 3001;
