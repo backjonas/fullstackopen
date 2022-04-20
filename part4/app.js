3f02d818acac97c4a48a3b0783a4cacab8c085e9
Jonas Bäck <jonas.c.back@gmail.com>
2022-04-20 11:01:42 +0300

    4.15

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ require('express-async-errors')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./routers/blogs')
+const userRouter = require('./routers/users')
 const logger = require('./utils/logger')
 
 
@@ -21,5 +22,6 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 module.exports = app
